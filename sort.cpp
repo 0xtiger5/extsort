c095435aa5ebfd255d7baae31e8aa44c56ed8d0f
0xtiger5 <3251406568@qq.com>
2022-06-03 15:10:15 +0800

    解决了一个bug，这个bug导致某些情况下输入输出数据不一致问题

--- sort.cpp
+++ sort.cpp
@@ -15,8 +15,8 @@
 #include "merge_sort.h"
 
 int main(int argc, char* argv[]) {
-    auto app = Merge_Sort(std::string("/data/int16GB.txt"), 5126320, 600);
-    // auto app = Merge_Sort(std::string("int"), 32602000, 60);
+    // auto app = Merge_Sort(std::string("/data/int16GB.txt"), 5126320, 600);
+    auto app = Merge_Sort(std::string("int"), 32602000, 60);
     // auto app = Merge_Sort(std::string("smallint"), 29343, 10);
     app.advance_merge_sort();
     return 0;
